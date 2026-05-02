4a9eb2f24d00dd6dbae52a73b3c75b52eb604d5e
rodrmart1024 <rodrmart1024@gmail.com>
2026-05-02 11:58:09 -0500

    Using Pyside6 instead of pygame for UI and Painter tools.

--- src/project.py
+++ src/project.py
@@ -1,1 +1,29 @@
-print("Hi")
+from PySide6 import QtWidgets, QtCore, QtGui
+
+
+def main():
+    pass
+
+def mondrian_inspired_art_ui():
+    pass
+
+class MondrianInspiredArt():
+
+    def meth_1():
+        pass
+
+
+    def meth_2():
+        pass
+
+
+if __name__ == "__main__":
+    main()
+
+# Using Pyside6 for the Widgets and Painter tools
+# Create the Window
+# Create the UI for all features
+# Create the Grid
+# Create the Squares by trying the usage of cells
+# Create the Color Schemes
+# Create the Signitures
